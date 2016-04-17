e0d3e81b30c1a43442eaa544828824be9a27c537
Jake <jcpenche@gmail.com>
2016-04-17 13:21:55 +0200

    first commit

---
+++ app/sorter.rb
@@ -0,0 +1,23 @@
+class Sorter < Struct.new(:list)
+
+  def simple(index = 0)
+    first = @list[index]
+    second = @list[index + 1]
+    first_index = @list.index(first)
+    second_index = @list.index(second)
+
+    if first > second
+      @list[second_index] = first
+      @list[first_index] = second
+      sort(@list, index + 1)
+    else
+
+    end
+  end
+end
+.
+[3,2,1]
+   .
+[2,3,1]
+     .
+[2,1,3]
---
+++ spec/sorter_spec.rb
@@ -0,0 +1,27 @@
+require_relative '../app/sorter.rb'
+
+RSpec.describe Sorter do
+  let(:sorted)   { [1,2,3,4,5,6,7,8,9,10] }
+  let(:unsorted) { sorted.to_a.sort_by{ rand } }
+  subject        { Sorter.new(unsorted) }
+
+  # Antes de empezar, leete esto https://en.wikipedia.org/wiki/Sorting_algorithm
+
+  before { expect_any_instance_of(Array).not_to receive(:sort) }
+
+  describe ".simple" do
+    it "returns a sorted list" do
+      expect(subject.simple).to eq(sorted)
+    end
+  end
+
+  describe "sorts lists" do
+    pending "using merge sort" do
+      expect(subject.merge_sort).to eq(sorted)
+    end
+
+    pending "using bubble sort" do
+      expect(subject.bubble_sort).to eq(sorted)
+    end
+  end
+end
